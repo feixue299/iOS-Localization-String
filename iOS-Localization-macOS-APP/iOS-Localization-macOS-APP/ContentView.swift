7f22e68f330dbd091c03d2874c81c2cb1befc7fa
feixue299 <12168109+feixue299@users.noreply.github.com>
2022-07-05 23:31:10 +0800

    create macOS APP

---
+++ iOS-Localization-macOS-APP/iOS-Localization-macOS-APP/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS-Localization-macOS-APP
+//
+//  Created by aria on 2022/7/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS-Localization-macOS-APP/iOS-Localization-macOS-APP/iOS_Localization_macOS_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Localization_macOS_APPApp.swift
+//  iOS-Localization-macOS-APP
+//
+//  Created by aria on 2022/7/5.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Localization_macOS_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
